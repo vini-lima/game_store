d6d51e540bbdf2379389d943c73575fcaa9a135a
vini-lima <vini20.henrique@gmail.com>
2021-09-26 22:29:26 -0300

    Jest and Storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wraper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de u átomo com react avançado escrito ao lado"
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typescript, ReactJS, NextJS e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Ilustration
       src="img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código"
